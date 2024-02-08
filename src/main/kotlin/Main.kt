f14cfa4bd33fd2d92a15b70de0ce95fb0dd6b59a
Sharon-Njunge <njungesharonwanjiku@gmail.com>
2024-02-08 07:42:34 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var name = "sharonwanjiku"
+    println(name)
+    var age = "23years"
+    println(age)
+    var number = "+254740571407"
+    println(number)
+    var weight = "60"
+    println(weight)
+    var citizen = "kenyan"
+    println(citizen)
+
+}
+
+
+
+
+)
+
+
+
+
